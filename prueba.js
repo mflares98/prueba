091c8375a6178425a3df5066d7647acd84f33ccf
mflares98 <74390852+mflares98@users.noreply.github.com>
2020-11-16 22:03:14 -0400

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,6 @@
+alert("Prueba de java");
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards',{ delay: 500 });
+ScrollReveal().reveal('.cards-banners-one',{ delay: 500 });
+ScrollReveal().reveal('.cards-banners-two',{ delay: 500 });
